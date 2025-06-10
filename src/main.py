f92184e9bee532bccecc5cb04a606b5fb2d923ea
Mauro_Abidal_Carrer <mauro@bib-batteries.fr>
2025-06-10 16:11:52 +0200

    WIP: Extracted the code block to create audio track inputs into a function. Switched from session_state variables to list of defaultdicts for the audio tracks inputs.

--- src/main.py
+++ src/main.py
@@ -1,32 +1,37 @@
 import os
 import tempfile
+from collections import defaultdict
 
 import streamlit as st
+from streamlit import session_state
 
 from video_editing import create_clip
+from config import DEFAULT_BPM, DEFAULT_DURATION
 
 
 def main():
     st.title("Video Clip Creator")
 
     # Create a temp dir for this session
-    if "tempdir" not in st.session_state:
-        st.session_state.tempdir = tempfile.TemporaryDirectory()
-        st.session_state.image_paths = []
-        st.session_state.prev_picture = None
+    if "tempdir" not in session_state:
+        session_state.tempdir = tempfile.TemporaryDirectory()
+        session_state.image_paths = []
+        session_state.prev_picture = None
+        # list of dicts containing keys file, bpm and duration
+        session_state.audio_tracks = []
 
-    tempdir = st.session_state.tempdir.name
-    image_paths = st.session_state.image_paths
+    tempdir = session_state.tempdir.name
+    image_paths = session_state.image_paths
 
     # Pictures
     st.subheader("Step 1: Capture Photos")
     # Reset pictures
     if st.button("Reset Photos"):
-        st.session_state.image_paths = []
+        session_state.image_paths = []
     # Take pictures
     picture = st.camera_input("Take a photo")
-    if picture is not None and st.session_state.prev_picture != picture:
-        st.session_state.prev_picture = picture
+    if picture is not None and session_state.prev_picture != picture:
+        session_state.prev_picture = picture
         img_idx = len(image_paths)
         image_path = os.path.join(tempdir, f"photo_{img_idx}.jpg")
         with open(image_path, "wb") as f:
@@ -35,23 +40,21 @@ def main():
         st.success(f"Captured photo #{img_idx + 1}")
         # Display nb of pictures taken
     st.write(f"{len(image_paths)} photo(s) taken.")
-
-    st.subheader("Step 2 & 3: Audio and Parameters")
-    col1, col2, col3 = st.columns([3, 1, 1])
-
-    with col1:
-        audio_file = st.file_uploader("Audio File")
-
-    with col2:
-        bpm = st.number_input("BPM", min_value=1.0, value=24.0)
-
-    with col3:
-        duration = st.number_input("Duration (s)", min_value=1.0, value=10.0)
-
-    if len(image_paths) == 0 or audio_file is None:
+    # Check that at least one picture has been taken
+    if len(image_paths) == 0:
         st.warning("Please take at least one photo and upload an audio file.")
         return
+    # Audio
+    st.subheader("Audio tracks")
+    create_audio_track_inputs(0)
+    track_has_file = lambda track: track["file"] is not None
+    if not all(map(track_has_file, session_state.audio_tracks)):
+        st.warning("Please provide an audio file for all the audio tracks.")
+        return
 
+    audio_file = session_state.audio_tracks[0]["file"]
+    bpm = session_state.audio_tracks[0]["bpm"]
+    duration = session_state.audio_tracks[0]["duration"]
     if st.button("Create Video"):
         with tempfile.NamedTemporaryFile(suffix=".mp4", delete=False) as out_file:
             audio_path = os.path.join(tempdir, audio_file.name)
@@ -71,6 +74,23 @@ def main():
                 st.video(f.read())
             st.download_button("Download Video", f, file_name="output.mp4")
             f.close()
+
+def create_audio_track_inputs(line_index: int):
+    audio_tracks = session_state.audio_tracks
+    print("len(audio_tracks):", len(audio_tracks), len(audio_tracks) <= line_index)
+    if len(audio_tracks) <= line_index:
+        print("Adding new audio track")
+        audio_track = defaultdict(file=None, bpm=DEFAULT_BPM, DEFAULT_DURATION=DEFAULT_DURATION)
+        audio_tracks.append(audio_track)
+    else:
+        audio_track = session_state.audio_tracks[line_index]
+    col1, col2, col3 = st.columns([3, 1, 1])
+    with col1:
+        audio_track["file"] = st.file_uploader("Audio File")
+    with col2:
+        audio_track["bpm"] = st.number_input("BPM", min_value=1.0, value=DEFAULT_BPM)
+    with col3:
+        audio_track["duration"] = st.number_input("Duration (s)", min_value=1.0, value=DEFAULT_DURATION)
 
 if __name__ == "__main__":
     main()
