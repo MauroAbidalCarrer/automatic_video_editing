e9abb6fcc9584853fe22a5362eb7d3c9ca7c8360
Mauro_Abidal_Carrer <mauro@bib-batteries.fr>
2025-06-10 15:13:45 +0200

    Fix: Fixed displayed number of pictures taken.

--- src/main.py
+++ src/main.py
@@ -18,13 +18,12 @@ def main():
     tempdir = st.session_state.tempdir.name
     image_paths = st.session_state.image_paths
 
+    # Pictures
     st.subheader("Step 1: Capture Photos")
-
-    if len(image_paths) > 0:
-        st.write(f"{len(image_paths)} photo(s) taken.")
-        if st.button("Reset Photos"):
-            st.session_state.image_paths = []
-
+    # Reset pictures
+    if st.button("Reset Photos"):
+        st.session_state.image_paths = []
+    # Take pictures
     picture = st.camera_input("Take a photo")
     if picture is not None and st.session_state.prev_picture != picture:
         st.session_state.prev_picture = picture
@@ -34,6 +33,8 @@ def main():
             f.write(picture.getbuffer())
         image_paths.append(image_path)
         st.success(f"Captured photo #{img_idx + 1}")
+        # Display nb of pictures taken
+    st.write(f"{len(image_paths)} photo(s) taken.")
 
     st.subheader("Step 2 & 3: Audio and Parameters")
     col1, col2, col3 = st.columns([3, 1, 1])
