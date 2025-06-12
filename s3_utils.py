2c0d4ddac5684a62fa1068a5a1e97c9f6b4ab8d9
Mauro_Abidal_Carrer <mauro@bib-batteries.fr>
2025-06-12 12:33:25 +0200

    Feat/Fix: Updated s3_utils to use streamlit.secrets instead dotenv/os.getenv(Feat). Fixed the url of the uploaded object(Fix).

--- s3_utils.py
+++ s3_utils.py
@@ -1,34 +1,36 @@
 from sys import argv
 from os import getenv
+from os.path import join
 
 import boto3
 from streamlit import secrets
-from dotenv import load_dotenv
-from botocore.client import Config
 
-load_dotenv()
 
-access_key = getenv("SCW_ACCESS_KEY")
-secret_key = getenv("SCW_SECRET_KEY")
-bucket_name = getenv("BUCKET_NAME")
+BUCKET_NAME = secrets["BUCKET_NAME"]
+REGION_NAME = secrets["REGION"]
+ENPOINT_URL = f"https://s3.{secrets['REGION']}.scw.cloud"
 s3_client = boto3.client(
     "s3",
-    region_name=region,
-    endpoint_url=f"https://s3.{getenv("REGION")}.scw.cloud",
-    aws_access_key_id=access_key,
-    aws_secret_access_key=secret_key,
-    # config=Config(signature_version="s3v4"),
+    region_name=REGION_NAME,
+    endpoint_url=ENPOINT_URL,
+    aws_access_key_id=secrets["SCW_ACCESS_KEY"],
+    aws_secret_access_key=secrets["SCW_SECRET_KEY"],
 )
 
-def upload_file_to_bucket(local_src_path: str, remote_dest_path: str):
-    s3_client.upload_file(local_src_path, bucket_name, remote_dest_path)
-    public_url = f"https://{bucket_name}.s3.{region}.scw.cloud/{remote_dest_path}"
-    print("Public URL:", public_url)
-    s3_client.put_object_acl(ACL="public-read", Bucket=bucket_name, Key=remote_dest_path)
-
+def upload_file_to_bucket(local_src_path: str, remote_dest_path: str) -> str:
+    """
+    ### Description:
+    Uploads a file to the bucket.
+    Sets it as public to make accessible.
+    Returns an url to access it.
+    """
+    s3_client.upload_file(local_src_path, BUCKET_NAME, remote_dest_path)
+    s3_client.put_object_acl(ACL="public-read", Bucket=BUCKET_NAME, Key=remote_dest_path)
+    return f"https://{BUCKET_NAME}.s3.{REGION_NAME}.scw.cloud/{remote_dest_path}" #join(ENPOINT_URL, remote_dest_path)
+# https://domnique-videos.s3.fr-par.scw.cloud/requirements.txt
 
 if __name__ == "__main__":
     if len(argv) < 3:
         print("Please provide a local_src_path and remote_dest_path as arguments.")
         exit(1)
-    upload_file_to_bucket(argv[1], argv[2])
+    print(upload_file_to_bucket(argv[1], argv[2]))
